6eb60d7281b5bcb36d253dd3a24e8e13a2ed7090
Geon <geonulim@gmail.com>
2024-10-08 23:17:07 +0900

    feat: 탐색 상단바 스크롤 시 고정되도록 구현

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -5,13 +5,17 @@ import { MOCK_CARDS } from 'features/explore/mock/cards';
 
 const ExplorePage = () => {
   return (
-    <div className='p-20 flex flex-col gap-20'>
-      <SearchBar />
-      <Tabs />
-      <Carousel.Rectangle title='오늘의 큐레이션' cards={MOCK_CARDS} />
-      <Carousel title='HOT' cards={MOCK_CARDS} />
-      <Carousel.Square title='곧 마감돼요!' cards={MOCK_CARDS} />
-      <Carousel title='꿀조합 컬렉션' cards={MOCK_CARDS} />
+    <div className='p-20 pt-0'>
+      <div className='flex flex-col gap-16 pb-12 sticky top-0 bg-background z-nav mb-12'>
+        <SearchBar />
+        <Tabs />
+      </div>
+      <main className='flex flex-col gap-20 overflow-y-auto'>
+        <Carousel.Rectangle title='오늘의 큐레이션' cards={MOCK_CARDS} />
+        <Carousel title='HOT' cards={MOCK_CARDS} />
+        <Carousel.Square title='곧 마감돼요!' cards={MOCK_CARDS} />
+        <Carousel title='꿀조합 컬렉션' cards={MOCK_CARDS} />
+      </main>
     </div>
   );
 };
